c482c4d2207a98b96bf419bd91a54bdabfa875fd
NADHIFA\Nadhifa <nadhifatiaraputri@gmail.com>
2018-05-09 13:47:30 +0700

    mengganti routes

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -11,4 +11,12 @@ $config['num_tag_close'] = '</span></li>';
 
 $config['cur_tag_open'] = '<li class="page-item active"><span class="page-link">';
 $config['cur_tag_close'] = '<span class="sr-only">(current)</span></span></li>';
+
+$config['next_link'] = '&gt;';
+$config['next_tag_open'] = '<li class="page-item"><span class="page-link">';
+$config['next_tag_close'] = '</span></li>';
+
+$config['prev_link'] = '&lt;';
+$config['prev_tag_open'] = '<li class="page-item"><span class="page-link">';
+$config['prev_tag_close'] = '</span></li>';
  ?>
